6c9e91fa051c95c2b2123da8c48f4970efc8674c
THARANI M <135812726+msdharani1@users.noreply.github.com>
2025-07-04 00:42:17 +0530

    Remove obfuscator plugin to fix ref2 function error

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import obfuscator from 'rollup-plugin-obfuscator'
 import { compression } from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    obfuscator({
-      globalOptions: {
-        debugProtection: true,
-        debugProtectionInterval: true,
-        stringArray: true,
-        stringArrayEncoding: ['base64'],
-        stringArrayThreshold: 0.8,
-      }
-    }),
     compression({ algorithm: 'gzip' })
   ],
   css: {
